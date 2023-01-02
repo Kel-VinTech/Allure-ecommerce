1f40206a4babd45a26b21c4b71c0be8fc27dff33
Kel-VinTech <mondaykelvintech@gmail.com>
2023-01-02 00:52:50 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import reactLogo from './assets/react.svg'
+import MainPage from './MainPage'
+
+function App() {
+
+  return (
+    <div>
+        <MainPage />
+    </div>
+  )
+}
+
+export default App
---
+++ src/MainPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MainPage = () => {
+    return (
+        <nav>
+            <h1>hello</h1>
+        </nav>
+    )
+}
+
+export default MainPage
