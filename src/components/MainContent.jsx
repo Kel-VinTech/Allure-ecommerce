002ceb5dcfa0d07ed2cc4a1fdeb62aef8ed7b463
Kel-VinTech <mondaykelvintech@gmail.com>
2023-01-15 03:16:51 -0600

    changes

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -23,6 +23,42 @@ const MainContent = () => {
                 <img className="image1" src={image1} />
             </div>
             </div>
+
+            <div className="content_2">
+                <h3>WHY YOU SHOULD CHOOSE ALLURE</h3>
+                <div className="table">
+                <table>
+                    <tr>
+                        <td>
+                            <h3>SKIN HEALTH</h3>
+                            <p>Our products contain only natural variants that are essential for your skin, oil and additives contained 
+                            in natural skincare have anti-inflammatory effects.</p>
+                        </td>
+                        <td></td>
+                        <td>
+                            <h3>SKIN HEALTH</h3>
+                            <p>Our products contain only natural variants that are essential for your skin, oil and additives contained 
+                            in natural skincare have anti-inflammatory effects.</p>
+                        </td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                    <td></td>
+                        <td>
+                            <h3>ORIGINALITY</h3>
+                            <p>Our products are unique, original
+                             and made carefully with good attention to details.</p>
+                        </td>
+                        <td></td>
+                        <td>
+                            <h3>GENTLE CARE</h3>
+                            <p>The products do not only cleanse the skin, but also protects 
+                            it from the negative effects of other unwanted environmental factors.</p>
+                        </td>
+                    </tr>
+                </table>
+                </div>
+            </div>
             
         </section>
     )
