f8e44f63466f2fd3e07304bed00ff21c5d512082
Kel-VinTech <mondaykelvintech@gmail.com>
2023-01-21 17:40:41 -0600

    changes

--- src/components/FooterContent.jsx
+++ src/components/FooterContent.jsx
@@ -21,32 +21,32 @@ const FooterContent = () => {
             <div className="foot_2 row">
                 <div className="foot_container">
                     <div className="foot_2_content">
-                    <div className="text">
-                        <h2>ALLURE.</h2>
-                        <p className="mess">Skin care is a science, Allure makes it art.
-                        <br/>Reduce fine lines & wrinkles with alpha hydroxy acid skin care products.
-                        Say HI to a new found glow routine.</p>
-                        <p className="copyright">Allurebeauty © 2023. All Rights Reserved.</p>
-                        <p className="copyright">Made by - Kelvin.eth</p>
-                    </div>
+                        <div className="text">
+                            <h2>ALLURE.</h2>
+                            <p className="mess">Skin care is a science, Allure makes it art.
+                            <br/>Reduce fine lines & wrinkles with alpha hydroxy acid skin care products.
+                            Say HI to a new found glow routine.</p>
+                            <p className="copyright">Allurebeauty © 2023. All Rights Reserved.</p>
+                            <p className="copyright">Made by - Kelvin.eth</p>
+                        </div>
                     
                         <div className="foot_line"></div>
 
-                    <div className="text_2">
-                        <div className="foot_links">
-                            <Link>Info</Link>
-                            <Link>Shipping & Returns</Link>
-                            <Link>Privacy Policy</Link>
-                            <Link>FAQs</Link>
-                            <Link>Contact</Link>
+                        <div className="text_2">
+                            <div className="foot_links">
+                                <Link>Info</Link>
+                                <Link>Shipping & Returns</Link>
+                                <Link>Privacy Policy</Link>
+                                <Link>FAQs</Link>
+                                <Link>Contact</Link>
+                            </div>
+                            <p>Soft Avenue, House 15, Ibeju <br/> Lekki,Lagos,Nigeria.</p>
+                            <div className="foot_icons">
+                                <FaTwitter className="twitter"/>
+                                <FaGithub />
+                                <FaLinkedin />
+                            </div>
                         </div>
-                        <p>Soft Avenue, House 15, Ibeju <br/> Lekki,Lagos,Nigeria.</p>
-                        <div className="foot_icons">
-                            <FaTwitter className="twitter"/>
-                            <FaGithub />
-                            <FaLinkedin />
-                        </div>
-                    </div>
                     </div>
                 </div>
     
