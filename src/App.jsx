eac81a1f63991ada3c479550a365f8648f73928a
Kel-VinTech <mondaykelvintech@gmail.com>
2023-02-01 11:41:30 -0600

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
 import React from 'react'
-import Footer from './components/Footer/FooterContent'
-import HeroContent from './components/HeroContent'
-import MainContent from './components/MainContent'
-import FooterContent from './components/Footer/FooterContent'
-// import { Route, Routes } from "react-router-dom"
-
+import Home from './pages/Home'
+import { Route, Routes,BrowserRouter } from "react-router-dom"
 import './scss/_reset.scss'
+
 function App() {
 
   return (
     <div className='container'>
-        <HeroContent/>
-        <MainContent />
-        <FooterContent />
+        
     </div>
   )
 }
