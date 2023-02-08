166ac1ce37fa469c114aa35dde18d760e555b5bb
Kel-VinTech <mondaykelvintech@gmail.com>
2023-02-07 18:28:59 -0600

    changes

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import App from './App'
 import * as ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 
 const rootElement = document.getElementById('root')
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
+    <BrowserRouter>
       <App />
+    </BrowserRouter>
   </React.StrictMode>
 )
