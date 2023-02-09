921f8de3a4cc1a1941b98f92827ef589deb35d79
Kel-VinTech <mondaykelvintech@gmail.com>
2023-02-09 16:51:43 -0600

    changes

--- src/components/BlogContent/index.jsx
+++ src/components/BlogContent/index.jsx
@@ -5,12 +5,16 @@ const BlogContent = () => {
     return (
         <main>
             <div className="row">
+                <div className="content">
                 <div className="Heading">
-                    <h1>Allure's Blog</h1>
+                    <h1>Allure’s Blog</h1>
                     <div className="Heading_icons">
                         <FaAngleLeft className="direction_icons left"/>
                         <FaAngleRight className="direction_icons right"/>
                     </div>
+                </div>
+                <p className="text">A close look at the skin care life and routine of celebrities, customers’ reviews about products,and inside scoop from our aesthetic experts.
+                </p>
                 </div>
             </div>
         </main>
