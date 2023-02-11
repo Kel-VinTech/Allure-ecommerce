0fba2fe52481f458765cf2e8ebcafe6389993fbe
Kel-VinTech <mondaykelvintech@gmail.com>
2023-02-11 16:34:25 -0600

    changes

--- src/components/BlogContent/index.jsx
+++ src/components/BlogContent/index.jsx
@@ -2,18 +2,6 @@ import "/src/scss/Blog.scss";
 import {FaAngleLeft, FaAngleRight} from 'react-icons/fa';
 import data from '/src/data/Blog.js';
 
-
-
-// Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
-
-// Import Swiper styles
-import 'swiper/scss';
-import 'swiper/scss/navigation';
-import 'swiper/scss/pagination';
-
-
 const BlogContent = (props) => {
     return (
         <main>
@@ -29,32 +17,9 @@ const BlogContent = (props) => {
                     <p className="text">A close look at the skin care life and routine of celebrities, customers’ reviews about products,and inside scoop from our aesthetic experts.
                     </p>
                 </div>
-                    <Swiper
-                        modules={[Navigation, Pagination, Scrollbar, A11y]}
-                        spaceBetween={100}
-                        slidesPerView={3}
-                        pagination={{ clickable: true }}
-                        scrollbar={{ draggable: true }}
-                        onSwiper={(swiper) => console.log(swiper)}
-                        onSlideChange={() => console.log('slide change')}
-                    >
-                    {data.map(user => (
-                        <SwiperSlide key={user.id} className="Slider row">
-                            <div className="data_container">
-                                <div className="user-image">
-                                    <img src="/src/assets/images/Rectangle 9.png" alt="yesy"/>
-                                </div>
-                                <h2>{user.title}</h2>
-                                <p>{user.text}</p>
-                            </div>
-                            
-                        </SwiperSlide>
-                    ))}
-                        
-                    </Swiper>
-            </div>
-        </main>
-    )
-}
+        </div>  
+     </main>
+ )}
+
 
 export default BlogContent;
