770bd4d2b88b39b33895f10d31e748e9b3b834b4
Kel-VinTech <mondaykelvintech@gmail.com>
2023-02-12 18:15:04 -0600

    can't figure out the cards

--- src/components/BlogContent/index.jsx
+++ src/components/BlogContent/index.jsx
@@ -1,55 +1,33 @@
 import "/src/scss/Blog.scss";
 import {FaAngleLeft, FaAngleRight} from 'react-icons/fa';
-import data from '/src/data/Blog.js';
+import BlogCards from '../BlogCards';
 
 import {Swiper, SwiperSlide} from 'swiper/react';
-import {FreeMode,Pagination} from 'swiper';
+import {FreeMode,Pagination,Scrollbar} from 'swiper';
 import 'swiper/scss';
 import "swiper/scss/free-mode";
 import 'swiper/scss/pagination';
 
 
-const BlogContent = (props) => {
+const BlogContent = () => {
+    // <div className="content">
+    //                 <div className="heading">
+    //                     <h1 className="heading_text">Allure’s Blog</h1>
+    //                     <div className="Heading_icons">
+    //                         {/* <FaAngleLeft className="direction_icons left"/>
+    //                         <FaAngleRight className="direction_icons right"/> */}
+    //                         <p>hello</p>
+    //                     </div>
+    //                 </div>
+    //                 <p className="text">A close look at the skin care life and routine of celebrities, customers’  reviews about products,and inside scoop from our aesthetic experts.
+    //                 </p>
+    //             </div>
+    
     return (
         <main>
             <div className="row">
-                <div className="content">
-                    <div className="Heading">
-                        <h1>Allure’s Blog</h1>
-                        <div className="Heading_icons">
-                            <FaAngleLeft className="direction_icons left"/>
-                            <FaAngleRight className="direction_icons right"/>
-                        </div>
-                    </div>
-                    <p className="text">A close look at the skin care life and routine of celebrities, customers’  reviews about products,and inside scoop from our aesthetic experts.
-                    </p>
-                </div>
-
-                    <div className="container row">
-                        <Swiper
-                            freeMode={true}
-                            grabCursor={true}
-                            modules={[FreeMode,Pagination]}
-                            slidesPerView={3}
-                            spaceBetween={40}
-                            pagination={{ clickable: true }}
-                            className="blog_swiper"
-                        >
-                            <SwiperSlide>
-                                <h1>side 1</h1>
-                            </SwiperSlide>
-                            <SwiperSlide>
-                                <h1>side 1</h1>
-                            </SwiperSlide>
-                            <SwiperSlide>
-                                <h1>side 1</h1>
-                            </SwiperSlide>
-                            <SwiperSlide>
-                                <h1>side 1</h1>
-                            </SwiperSlide>
-                        </Swiper>
-                    </div>
-            </div>  
+                
+         </div>
         </main>
  )}
 
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
-
+export {default as BlogCards} from "./BlogCards";
 export { default as MainContent } from "./MainContent";
 export { default as BlogContent} from "./BlogContent";
 export { default as HeroContent} from "./HeroContent";
 export { default as Footer } from "./Footer";
+
