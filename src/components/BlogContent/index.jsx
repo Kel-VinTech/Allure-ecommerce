b12874385502d3349bb3327f28f9799b998e2997
Kel-VinTech <mondaykelvintech@gmail.com>
2023-02-16 17:45:29 -0600

    changes

--- src/components/BlogContent/index.jsx
+++ src/components/BlogContent/index.jsx
@@ -3,43 +3,51 @@ import {FaAngleLeft, FaAngleRight} from 'react-icons/fa';
 import data from '/src/data/Blog.js';
 
 import {Swiper, SwiperSlide} from 'swiper/react';
-import {FreeMode,Pagination,Scrollbar} from 'swiper';
+import {Pagination} from 'swiper';
 import 'swiper/scss';
 import "swiper/scss/free-mode";
 import 'swiper/scss/pagination';
 
 
 const BlogContent = (props) => {
-    // <div className="content">
-    //                 <div className="heading">
-    //                     <h1 className="heading_text">Allure’s Blog</h1>
-    //                     <div className="Heading_icons">
-    //                         {/* <FaAngleLeft className="direction_icons left"/>
-    //                         <FaAngleRight className="direction_icons right"/> */}
-    //                         <p>hello</p>
-    //                     </div>
-    //                 </div>
-    //                 <p className="text">A close look at the skin care life and routine of celebrities, customers’  reviews about products,and inside scoop from our aesthetic experts.
-    //                 </p>
-    //             </div>
+    
     
     return (
         <main>
+          <div className="content">
+                    <div className="heading">
+                        <h1 className="heading_text">Allure’s Blog</h1>
+                        <div className="Heading_icons">
+                            {/* <FaAngleLeft className="direction_icons left"/>
+                            <FaAngleRight className="direction_icons right"/> */}
+                            <p>hello</p>
+                        </div>
+                    </div>
+                    <p className="text">A close look at the skin care life and routine of celebrities, customers’  reviews about products,and inside scoop from our aesthetic experts.
+                    </p>
+                </div>
+
             <Swiper className="container_card row"
                 modules={[Pagination]}
-                spaceBetween={25}
+                spaceBetween={30}
                 slidesPerView={3}
                 pagination={{ clickable: true }}
-                onSlideChange={() => console.log('slide change')}
-                onSwiper={(swiper) => console.log(swiper)}
                 breakpoints={{
                     0: {
                       slidesPerView: 1,
+                    },
+                    480: {
+                      slidesPerView: 1,
+                    },
+                    768: {
+                      
+                      slidesPerView: 2,
                       spaceBetween:30,
                     },
-                    450: {
+                    1024: {
                       
-                      slidesPerView: 1,
+                      slidesPerView: 3,
+                      spaceBetween:60,
                     },
                   }}
             >
