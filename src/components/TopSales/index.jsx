08b1763f5743c971c8533690dc76f66b9f12f11a
Kel-VinTech <mondaykelvintech@gmail.com>
2023-02-18 03:07:39 -0600

    added swiper for new page

--- src/components/TopSales/index.jsx
+++ src/components/TopSales/index.jsx
@@ -1,18 +1,71 @@
 import "/src/scss/TopSales.scss";
+import data from '/src/data/TopSales.js';
+import { Link } from "react-router-dom";
 import { MdChevronLeft,MdChevronRight, MdEast } from "react-icons/md";
+import {Swiper, SwiperSlide} from 'swiper/react';
+import {Pagination} from 'swiper';
+import 'swiper/scss';
+import "swiper/scss/free-mode";
+import 'swiper/scss/pagination';
 
-const TopSales = () => {
+const TopSales = (props) => {
     return (
         <main>
             <div className="heading-container">
                 <h2>Top picks from <s>SALES</s> </h2>
-                <div className="Heading_icons">
+                <div className="Heading_icon">
                         <div className="directions-icon">
                           <MdChevronLeft className="direction_icons left"/>
                           <MdChevronRight className="direction_icons right"/> 
                         </div>  
                 </div>
             </div>
+            <Swiper className="container_card row"
+                modules={[Pagination]}
+                spaceBetween={30}
+                slidesPerView={4}
+                pagination={{ clickable: true }}
+                breakpoints={{
+                    0: {
+                      slidesPerView: 1,
+                      spaceBetween:30,
+                    },
+                    500: {
+                      slidesPerView: 1,
+                      spaceBetween:30,
+                    },
+                    648: {
+                      slidesPerView: 2,
+                      spaceBetween:30,
+                    },
+                    768: {
+                      
+                      slidesPerView: 2,
+                      spaceBetween:30,
+                    },
+                    1024: {
+                      
+                      slidesPerView: 3,
+                      spaceBetween:60,
+                    },
+                  }}
+            >
+                {data.map(item => (
+                    <SwiperSlide key={item.id} className="single_container">
+                    <div className="swiper-image">
+                        <img src={item.image} alt="image" />
+                    </div>
+                    <h3>{item.title}</h3>
+                    <div className="swiper-prices">
+                        <span>${item.price1}</span>
+                        <span><s>${item.price2}</s></span>
+                    </div>
+                    <div className="add-link">
+                      <Link to="#">Add to cart <MdEast/> </Link>
+                  </div>
+                </SwiperSlide>
+                ))}
+            </Swiper>
         </main>
     )
 }
---
+++ src/data/TopSales.js
@@ -0,0 +1,86 @@
+import image1 from "/src/assets/images/Rectangle 12.png"
+
+const data = [
+    {
+        id:1,
+        image: image1,
+        title: "Moisturizers",
+        text: "Moisturizers",
+        price1: 30,
+        price2: 60
+    },
+    {
+        id:2,
+        image: image1,
+        title: "Cleansers",
+        text: "Moisturizers",
+        price1: 30,
+        price2: 60
+    },
+    {
+        id:3,
+        image: image1,
+        title: "Moisturizers",
+        text: "Moisturizers",
+        price1: 30,
+        price2: 60
+    },
+    {
+        id:4,
+        image: image1,
+        title: "Cleansers",
+        text: "Moisturizers",
+        price1: 30,
+        price2: 60
+    },
+    {
+        id:5,
+        image: image1,
+        title: "Moisturizers",
+        text: "Moisturizers",
+        price1: 30,
+        price2: 60
+    },
+    {
+        id:6,
+        image: image1,
+        title: "Moisturizers",
+        text: "Moisturizers",
+        price1: 30,
+        price2: 60
+    },
+    {
+        id:7,
+        image: image1,
+        title: "Moisturizers",
+        text: "Moisturizers",
+        price1: 30,
+        price2: 60
+    },
+    {
+        id:8,
+        image: image1,
+        title: "Moisturizers",
+        text: "Moisturizers",
+        price1: 30,
+        price2: 60
+    },
+    {
+        id:9,
+        image: image1,
+        title: "Moisturizers",
+        text: "Moisturizers",
+        price1: 30,
+        price2: 60
+    },
+    {
+        id:10,
+        image: image1,
+        title: "Moisturizers",
+        text: "Moisturizers",
+        price1: 30,
+        price2: 60
+    }
+]
+
+export default data;
