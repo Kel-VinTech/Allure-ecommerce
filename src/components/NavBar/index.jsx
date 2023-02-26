5a41c4732a59d40eb25f4df7373af3ef9514bbb2
Kel-VinTech <mondaykelvintech@gmail.com>
2023-02-26 08:51:09 -0500

    changes

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -5,7 +5,7 @@ const NavBar =() => {
     return (
         <div className="navbar row">
             <Logo />
-
+            <h2>Cart<span>(2)</span></h2>
             <nav className="nav_links">
             
             </nav>
