259c0e91a2c2ee65eb72b7c7f1fa22154656ddff
Kel-VinTech <mondaykelvintech@gmail.com>
2023-02-28 18:05:57 -0500

    changes

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,17 +1,24 @@
 import "/src/scss/Navbar.scss";
 import Logo from "../Logo";
 import ProductCart from "../ProductCart";
+import { Link } from "react-router-dom";
 
 const NavBar =() => {
     return (
         <div className="navbar row">
-            <div className="logo">
             <Logo />
-            </div>
             <nav className="nav_links">
-            
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/">Products</Link></li>
+                    <li><Link className="sales" to="/">SALES</Link></li>
+                    <li><Link to="/">Blog</Link></li>
+                    <li><Link to="/">About Us</Link></li>
+                </ul>
             </nav>
-            <ProductCart />
+            <div className="secondary_link">
+                <ProductCart />
+            </div>
         </div>
     )
 }
