ad8bef69cbb56fd972384734ed1c832c60634f10
Kel-VinTech <mondaykelvintech@gmail.com>
2023-03-01 19:17:32 -0500

    added bnew div

--- src/components/HeroContent/index.jsx
+++ src/components/HeroContent/index.jsx
@@ -1,30 +1,30 @@
 import React from "react";
-import {FaUser,FaBars,FaCartPlus,FaHeart, FaSearch} from 'react-icons/fa';
+import { Link } from "react-router-dom";
+import { MdEast } from "react-icons/md";
 import '/src/scss/Hero.scss'
 
 const HeroContent = () => {
     return (
     <section className="hero row"
     >
-        {/* <nav>
-            
-           <div className="icons">
-                <FaSearch className="icon"/>
-                <FaHeart className=" heart"/>
-                <FaCartPlus className="icons"/> 
-                <FaUser className="icons"/>
-            </div>
-            <FaBars className=" menu"/>
-        </nav> */}
+     
         <div className="hero-content">
             <h1 className="hero-title">Allure : Skin for days</h1>
             <p className="hero-text">Skin care is a science, Allure makes it art. Reduce fine lines & <br/> wrinkles with alpha hydroxy acid skin care products. Say HI to a new found glow routine.</p>
              <button>Shop now</button>
         </div>
-        {/* <div className="products_collection">
-           <h2>All</h2>
-           <h2>cream</h2>  
-        </div> */}
+        <div className="products_collection">
+           <ul>
+                    <li><Link to="#">All</Link></li>
+                    <li><Link to="#">Moisturizers</Link></li>
+                    <li><Link to="#">Treatments</Link></li>
+                    <li><Link to="#">Cleansers</Link></li>
+                    <li><Link to="#">Masks</Link></li>
+                    <li><Link to="#">Lip Balms</Link></li>
+                    <li><Link to="#">Wellness</Link></li>
+                    <li><Link to="#"><MdEast/> </Link></li>
+            </ul>  
+        </div>
     </section>   
     )
 }
