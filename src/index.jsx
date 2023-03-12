cebfa106c2c5a9d5d732acb4e33f6247e6b91242
Kel-VinTech <mondaykelvintech@gmail.com>
2023-03-11 19:12:11 -0500

    created redux

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
+import { Provider } from 'react-redux';
 import App from './App'
 import * as ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import store from "../src/redux/store"
+
+
 
 const rootElement = document.getElementById('root')
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <BrowserRouter>
+    <Provider store={store}>
       <App />
+    </Provider>
     </BrowserRouter>
   </React.StrictMode>
 )
