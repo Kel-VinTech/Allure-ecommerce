e7b53ce85e1fc5189a2fc291c6e651d3961967b8
Kel-VinTech <mondaykelvintech@gmail.com>
2023-03-27 17:10:50 -0400

    changes

--- src/components/MainContent/index.jsx
+++ src/components/MainContent/index.jsx
@@ -4,28 +4,38 @@ import {FaAngleLeft, FaAngleRight} from 'react-icons/fa';
 import eli7 from '/src/assets/images/Ellipse 8.png';
 import image1 from '/src/assets/images/Rectangle 4.png'
 import "/src/scss/Main.scss";
+import { motion } from "framer-motion";
+
 
 const MainContent = () => {
     return(
         <main className="row main">
             <div className="main-content">
-                <img className="elipse" src={eli7} />
-                <img className="elipse7" src={eli7} />
+                <motion.img className="elipse" src={eli7}
+                initial={{x:-300}}
+                animate={{x:0}} />
+                <motion.img className="elipse7" src={eli7}
+                initial={{x:-300}}
+                animate={{x:0}} />
                 
                 
                <div className="content-message">
                     <div className="content">
-                        <div className="content-text">
+                        <motion.div className="content-text"
+                        initial={{x:-300}}
+                        animate={{x:0}}>
                         <h4>ABOUT US</h4>
                             <h2>EARTH IS OUR <br/>TRUE SOURCE <br/>OF BEAUTY</h2>
                             <p>Allure is a cosmetic brand of face <br/> oils,body care and wellness products <br/> for all skin types. </p>
-                        </div>
+                        </motion.div>
                     </div>
                     <button>LEARN MORE</button>
-                    <div className="message">
+                    <motion.div className="message"
+                    initial={{x:300}}
+                    animate={{x:0}}>
                         <p>We are a brand and representation <br/> for everyone  who believes that <br/> beauty does not come from a <br/> snapshot, but from a sequence.</p>
                         <img className="image1" src={image1} />
-                    </div>
+                    </motion.div>
                </div>
             </div>
 
