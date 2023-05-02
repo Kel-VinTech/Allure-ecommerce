fae7127d6fbc2671c518ef39e138df44b0630b13
Kel-VinTech <mondaykelvintech@gmail.com>
2023-05-01 21:58:48 -0400

    made product id

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,16 +1,34 @@
 import { useParams } from "react-router-dom"
 import products from '/src/data/products.js';
+import "/src/scss/ProductDetails.scss";
   
 
 const ProductDetails =() =>{
     const {id} = useParams();
-    const product = products.find((item) => item.id === id);
-    // const {productName, imgUrl, price, shortDesc, description} = product
+    const product = products.find(item => item.id === id);
+    const {productName, imgUrl, price, shortDesc, description} = product
 
     return(
-        <div className="this">
-            <h1>productName</h1>
-        </div>
+        <main className="row">
+            <div className="cart-container">
+                <div className="cart-image">
+                    <img src={imgUrl} alt="" />
+                </div>
+                <div className="cart-text">
+                    <h1>{productName}</h1>
+                    <h3>${price}.00</h3>
+                    <hr />
+                    <p className="note">
+                        {shortDesc}
+                    </p>
+                    <hr />
+                    <p className="description">
+                        {description}
+                    </p>
+                    <hr />
+                </div>
+            </div>
+        </main>
     )
 }
 
