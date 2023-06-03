38c1a9e6dda0d30369571e8eb9d0f92bc46e6305
Kel-VinTech <mondaykelvintech@gmail.com>
2023-06-03 10:49:30 -0400

    changes

--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Cart = () => {
     return <main>
         <div className="cart">
     <div className="cart-link">
-        
+        <Link to="#"></Link>
     </div>
         </div>
     </main>
