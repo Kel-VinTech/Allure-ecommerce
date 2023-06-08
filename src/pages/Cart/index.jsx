c73b820b0b1003e044f62070133f08bb01d9c9a8
Kel-VinTech <mondaykelvintech@gmail.com>
2023-06-08 11:18:11 -0400

    made cart heading

--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -5,8 +5,8 @@ import "/src/scss/Cart.scss";
 const Cart = () => {
     return <main>
         <div className="cart">
-            <div className="cart-link">
-                <div className="links">
+            <div className="cart-heading">
+                <div className="cart-links">
                 <Link to="#">Shopping cart</Link>
                 <Link to="#">Shipping details</Link>
                 <Link to="#">Payment details</Link>
