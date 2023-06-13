8221799aa8ce5bca0e8708e35e74a3d48e8da7c2
Kel-VinTech <mondaykelvintech@gmail.com>
2023-06-13 14:20:32 -0400

    changes

--- src/pages/Payment/index.jsx
+++ src/pages/Payment/index.jsx
@@ -36,15 +36,66 @@ const Payment = () => {
                     </div>
                     <div className="cart-line"></div>               
                 </div>
-                <div className="payment-text row">
-                    <h2>Payment method</h2>
-                    <span><BiLock/><p>Secure server</p></span>
+                <div className="payment-information row">
+                    <div className="payment-text">
+                        <h2>Payment method</h2>
+                        <span><BiLock/><p>Secure server</p></span>
+                    </div>
+
+
+                    <div className="payment-items">
+                        <div className="card-details">
+                            <div className="card-details_head">
+                                <div className="logo">
+                                    <span>
+                                    <BiLock/>
+                                    <BiLock/>
+                                    </span>
+                                    <span>
+                                    <BiLock/>
+                                    <BiLock/>
+                                    </span>
+                                </div>
+                                <p>Safe money transfer using your bank account;Visa , Mastercard or Verve.</p>
+                            </div>
+
+                        </div>
+                        <div className="info-summary">
+                            <h2>Payment Summary</h2>
+
+                            <div className="cart-line3"></div>
+
+                            <p>23, Abraham Julie Street, Opp. GLK Church, IKeja, Lagos.</p>
+
+                            <div className="cart-line3"></div>
+
+                            <p>Expected arrival date: Between 22nd September and 26th September 2023</p>
+
+                            <div className="cart-line3"></div>
+
+                                    <div className="cart-foot-info">
+                                <div className="cart-checkout2">
+                                        <div className="checkout-details">
+                                            <div className="text">
+                                                <p>Products in cart :</p>
+                                                <p className="text-info-right">{totalQuantity} items</p>
+                                            </div>
+                                            <div className="text">
+                                                <p>Shipping :</p>
+                                                <p className="text-info-right">$2.50</p>
+                                            </div>
+                                            <div className="text">
+                                                <p>Total :</p>
+                                                <p className="text-info-right">${totalAmount}.00</p>
+                                            </div>
+                                        </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
-                <div className="payment-items">
-                    <div className="card-details"></div>
-                    <div className="info-summary"></div>
-                </div>
+                
             </div>
         </main>
     )
