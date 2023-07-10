bb24e313b92a2050a671d94f540fd39f2bd1d3fe
Kel-VinTech <mondaykelvintech@gmail.com>
2023-07-09 21:31:41 -0400

    changed images

--- src/data/products.js
+++ src/data/products.js
@@ -1,4 +1,18 @@
-import productImg01 from "/src/assets/images/Rectangle 31.jpg"
+import productImg01 from "/src/assets/images/lipcare.jpg"
+
+// moisturizers
+import productImg02 from "/src/assets/images/lina-verovaya-9s9fiLtwauw-unsplash.jpg";
+import productImg03 from "/src/assets/images/mosturizer2.jpg";
+import productImg04 from "/src/assets/images/mosturizer3.jpg";
+
+// body glow
+import productImg05 from "/src/assets/images/shower gel.jpg";
+import productImg06 from "/src/assets/images/glow2.jpg";
+import productImg07 from "/src/assets/images/glow8.jpg";
+
+// scrub
+import productImg08 from "/src/assets/images/scrub2.jpg";
+import productImg09 from "/src/assets/images/scrub1.jpg";
 
 const products = [
     {
@@ -22,7 +36,7 @@ const products = [
     {
     id: "02",
     productName: "Revox  Cleanser",
-    imgUrl: productImg01,
+    imgUrl: productImg08,
     category: "body scrub",
     price: 45.00,
     shortDesc:
@@ -39,7 +53,7 @@ const products = [
   {
     id: "03",
     productName: "Curology Face Cream",
-    imgUrl: productImg01,
+    imgUrl: productImg02,
     category: "moisturizers",
     price: 64.00,
     shortDesc:
@@ -56,7 +70,7 @@ const products = [
   {
     id: "04",
     productName: "Avant Shower Gel",
-    imgUrl: productImg01,
+    imgUrl: productImg05,
     category: "glow recipe",
     price: 139.00,
     shortDesc:
@@ -73,7 +87,7 @@ const products = [
   {
     id: "05",
     productName: "Stone and Beam Westview ",
-    imgUrl: productImg01,
+    imgUrl: productImg03,
     category: "moisturizers",
     price: 139.00,
     shortDesc:
@@ -90,8 +104,8 @@ const products = [
   {
     id: "06",
     productName: "Coco Body Oil",
-    imgUrl: productImg01,
-    category: "body scrub",
+    imgUrl: productImg06,
+    category: "glow",
     price: 84.00,
     shortDesc:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!",
@@ -107,8 +121,8 @@ const products = [
   {
     id: "07",
     productName: "Allie Rosewater",
-    imgUrl: productImg01,
-    category: "glow",
+    imgUrl: productImg09,
+    category: "body scrub ",
     price: 139.00,
     shortDesc:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!",
@@ -124,7 +138,7 @@ const products = [
   {
     id: "08",
     productName: "Avocado Face Oil ",
-    imgUrl: productImg01,
+    imgUrl: productImg07,
     category: "glow",
     price: 65.00,
     shortDesc:
