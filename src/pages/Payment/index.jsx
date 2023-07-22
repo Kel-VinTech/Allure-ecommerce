395a7e561bc7a41ba908dbc8dab8ecc00fb69c84
Kel-VinTech <mondaykelvintech@gmail.com>
2023-07-22 16:12:57 -0400

    changes

--- src/pages/Payment/index.jsx
+++ src/pages/Payment/index.jsx
@@ -23,6 +23,7 @@ const Payment = () => {
         }
     }
 
+    const newLocal = "payment-information row";
     return (
         <main>
             <div className="cart-check row">
@@ -41,64 +42,66 @@ const Payment = () => {
                     </div>
                     <div className="cart-line"></div>               
                 </div>
-                <div className="payment-information row">
+                <div className={newLocal}>
                     <div className="payment-text">
                         <h2>Payment method</h2>
                         <span><BiLock/><p>Secure server</p></span>
                     </div>
 
 
-                    <div className="payment-items">
-                        <div className="card-details">
-                            <div className="card-details_head">
-                                <div className="logo">
-                                    <span>
-                                    <img className="circle_card" src={carbon} alt="" />
-                                    <p>Credit card</p>
-                                    </span>
-                                    <div className="card-images">
-                                        <img className="card_image" src={master} alt="" />
-                                        <img className="card_image" src={visa} alt="" />
-                                    </div>
-                                </div>
-                                <p className="describe">Safe money transfer using your bank 
-                                account;Visa , Mastercard or Verve.</p>
-
-                                <form action="#">
-                                    
-                                </form>
-                            </div>
-
-                        </div>
-                        <div className="info-summary">
-                            <h2>Payment Summary</h2>
-
-                            <div className="cart-line3"></div>
-
-                            <p>23, Abraham Julie Street, Opp. GLK Church, IKeja, Lagos.</p>
-
-                            <div className="cart-line3"></div>
-
-                            <p>Expected arrival date: Between 22nd September and 26th September 2023</p>
-
-                            <div className="cart-line3"></div>
-
-                                    <div className="cart-foot-info">
-                                <div className="cart-checkout2">
-                                        <div className="checkout-details">
-                                            <div className="text">
-                                                <p>Products in cart :</p>
-                                                <p className="text-info-right">{totalQuantity} items</p>
-                                            </div>
-                                            <div className="text">
-                                                <p>Shipping :</p>
-                                                <p className="text-info-right">$2.50</p>
-                                            </div>
-                                            <div className="text">
-                                                <p>Total :</p>
-                                                <p className="text-info-right">${totalAmount}.00</p>
-                                            </div>
+                    <div className="payment-content">
+                        <div className="payment-items">
+                            <div className="card-details">
+                                <div className="card-details_head">
+                                    <div className="logo">
+                                        <span>
+                                        <img className="circle_card" src={carbon} alt="" />
+                                        <p>Credit card</p>
+                                        </span>
+                                        <div className="card-images">
+                                            <img className="card_image" src={master} alt="" />
+                                            <img className="card_image" src={visa} alt="" />
                                         </div>
+                                    </div>
+                                    <p className="describe">Safe money transfer using your bank 
+                                    account;Visa , Mastercard or Verve.</p>
+
+                                    <form action="#">
+                                        
+                                    </form>
+                                </div>
+
+                            </div>
+                            <div className="info-summary">
+                                <h2>Payment Summary</h2>
+
+                                <div className="cart-line3"></div>
+
+                                <p>23, Abraham Julie Street, Opp. GLK Church, IKeja, Lagos.</p>
+
+                                <div className="cart-line3"></div>
+
+                                <p>Expected arrival date: Between 22nd September and 26th September 2023</p>
+
+                                <div className="cart-line3"></div>
+
+                                        <div className="cart-foot-info">
+                                    <div className="cart-checkout2">
+                                            <div className="checkout-details">
+                                                <div className="text">
+                                                    <p>Products in cart :</p>
+                                                    <p className="text-info-right">{totalQuantity} items</p>
+                                                </div>
+                                                <div className="text">
+                                                    <p>Shipping :</p>
+                                                    <p className="text-info-right">$2.50</p>
+                                                </div>
+                                                <div className="text">
+                                                    <p>Total :</p>
+                                                    <p className="text-info-right">${totalAmount}.00</p>
+                                                </div>
+                                            </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
